cf0837ffd194ca0835a7c3000fab8cb25d9ac923
eric-klimowich <eklimowich@hotmail.com>
2018-10-01 13:15:37 -0400

    My Yelp

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,22 @@
 class Review
-  
+  attr_accessor :rating, :content
+  attr_reader :customer, :restaurant
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(customer, restaurant, rating, content)
+    @customer = customer
+    @restaurant = restaurant
+    @rating = rating
+    @content = content
+    @@all << self
+  end
+
 end
 
+# - `Review#rating`
+#   - returns the star rating for a restaurant. This should be an integer from 1-5
